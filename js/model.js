0524de65413252f715c69d1fa0516bf692f47105
João Magalhães <joamag@gmail.com>
2021-04-08 13:23:21 +0100

    feat: support for new model methods

--- js/model.js
+++ js/model.js
@@ -11,6 +11,30 @@ export const ModelAPI = superclass =>
             const response = await this.get(url);
             return response;
         }
+
+        async createEntity(model, payload) {
+            const url = `${this.baseUrl}api/admin/models/${model}`;
+            const response = await this.post(url, { dataJ: payload });
+            return response;
+        }
+
+        async getEntity(model, _id) {
+            const url = `${this.baseUrl}api/admin/models/${model}/${_id}`;
+            const response = await this.get(url);
+            return response;
+        }
+
+        async updateEntity(model, _id, payload) {
+            const url = `${this.baseUrl}api/admin/models/${model}/${_id}`;
+            const response = await this.put(url, { dataJ: payload });
+            return response;
+        }
+
+        async deleteEntity(model, _id) {
+            const url = `${this.baseUrl}api/admin/models/${model}/${_id}`;
+            const response = await this.delete(url);
+            return response;
+        }
     };
 
 export default ModelAPI;
